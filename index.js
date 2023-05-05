bc6c749565361110c83effb54eb381dafaa9806f
Agunt3 <127569071+Agunt3@users.noreply.github.com>
2023-05-04 21:52:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,47 +4,44 @@ const fs = require('fs');
 const {MongoClient} = require('mongodb');
 const uri ="mongodb+srv://akhila:guntupalli@cluster0.zed8ykx.mongodb.net/test";
 const client = new MongoClient(uri);
-const server = http.createServer((req, res)=>{
 
+const DBconnect=async()=>{
+    try{
+        await client.connect();
+        console.log("db connected")
+    
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+DBconnect();
+
+const server = http.createServer((req, res)=>{
+    const headers =  {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+  };
     console.log(req.url);
-    // / or /api /about.html
-     
     if( req.url ==='/'){
      
         fs.readFile(path.join(__dirname,'public','index.html'),
         (err, content)=>{
-
             if (err) throw err;
             res.writeHead(200,{ 'Content-type': 'text/html'});
             res.end(content);
 
         });
-
-
     }
-    else if(req.url =='/about.html'){
-
-       
-        fs.readFile(path.join(__dirname,'public','about.html'),
-        (err, content)=>{
-            if(err ) throw err;
-            res.writeHead(200, { 'Content-type': 'text/html'})
-            res.end(content)
-        });
-
-
-
-    }
+    
     else if(req.url =='/api'){
 
         fs.readFile(path.join(__dirname,'public','db.json'), 'utf-8',
         (err, content)=>{
-
             if(err ) throw err ;
-            res.writeHead(200, { 'Content-type': 'application/json'})  
+            res.writeHead(200, headers)  
             res.end(content);
         });
-
 
     }
 
